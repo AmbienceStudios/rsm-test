82dd8b837eff4105bb98f78befc71399d43815be
AmbienceStudios <Stanton.Deron@icloud.com>
2023-04-13 00:44:42 -0700

    Organized Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar.jsx'
 import Welcome from './components/Welcome'
 
 function App() {
@@ -10,10 +7,7 @@ function App() {
 
   return (
     <div className="container">
-      {/* <Navbar/> */}
-      <Welcome/>
-
-    
+      <Welcome/>    
     </div>
         
 
